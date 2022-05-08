74c8310a08423588b143ddbe02ebf7c5eadc1602
backchristoffer <backchristoffer@hotmail.com>
2022-05-09 00:50:35 +0200

    set up build test

---
+++ test/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+cd ..
+echo "Stopping ocp4test container"
+docker stop ocp4test
+echo "Removing old ocp4test container"
+docker rm ocp4test
+echo "Building ocp4-release-tool"
+docker build -t ocp4-release-tool .
+echo "Starting container ocp4test"
+docker run --name ocp4test -d -p 5000:5000 ocp4-release-tool
+echo "Wait 3sec for container to start"
+sleep 3
+echo "Checking if container is running"
+if curl -L -X GET http://localhost:5000; then 
+    echo "Test pass"
+else 
+    echo "Test fail"
+fi;
