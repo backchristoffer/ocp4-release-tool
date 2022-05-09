0ff7c3824425b59fa1a4e79e13f2c28d5f96176b
backchristoffer <backchristoffer@hotmail.com>
2022-05-09 07:45:18 +0200

    Formatting

--- test/build.sh
+++ test/build.sh
@@ -2,6 +2,7 @@
 set -e
 
 cd ..
+
 if docker ps -q -f name=ocp4test; then
     echo "Stopping ocp4test container"
     docker stop ocp4test
@@ -10,6 +11,7 @@ if docker ps -q -f name=ocp4test; then
 else
     echo "No ocp4test running"
 fi;
+
 echo "Building ocp4-release-tool"
 docker build -t ocp4-release-tool .
 echo "Starting container ocp4test"
@@ -17,6 +19,7 @@ docker run --name ocp4test -d -p 5000:5000 ocp4-release-tool
 echo "Wait 3sec for container to start"
 sleep 3
 echo "Checking if container is running"
+
 if curl -L -X GET http://localhost:5000; then 
     echo "Test pass"
 else 
