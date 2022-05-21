73e9ff9a35737f02dcbd1f719f022bd7d488644e
backchristoffer <backchristoffer@hotmail.com>
2022-05-21 13:04:01 +0200

    remove test for auth - need to mock

--- src/errata_api.py
+++ src/errata_api.py
@@ -9,8 +9,3 @@ class errata():
     
     def request(self):
         return requests.get(self.url, headers={'Authorization': 'access_token %s'.format(self.token)})
-
-
-err = errata(os.getenv('ERRATA_URL_OPENSHIFT'), os.getenv('TOKEN'))
-
-print(err.request().json())
--- tests/test_errata_api.py
+++
@@ -1,6 +0,0 @@
-from src.errata_api import errata
-import os
-errata_r = errata(os.getenv('SUPPORT_URL'), os.getenv('TOKEN'))
-
-def test_errata_request():
-    assert errata_r.request() == 200
